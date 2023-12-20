30c35e267ce17d3d6474f7e399839df3cb58666e
unknown <amintea@mail.ru>
2023-12-21 00:03:06 +0700

    penambahan tombol kembali ke atas

--- js/script.js
+++ js/script.js
@@ -70,3 +70,23 @@ function resetAll(){
   document.getElementById("kelamin").innerHTML = null;
   document.getElementById("pesan").innerHTML = null;
 }
+
+// Kembali Ke Atas //
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
